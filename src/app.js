6cea43a61be9c43a356c65ba33935a3f81859215
OzzieWP <ozziewpcontact@gmail.com>
2025-03-11 00:10:47 +0000

    Project done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  let whoRandom = who[Math.floor(Math.random() * who.length)]
+  let actionRandom = action[Math.floor(Math.random() * action.length)]
+  let whatRandom = what[Math.floor(Math.random() * what.length)]
+  let whenRandom = when[Math.floor(Math.random() * when.length)]
+  // console.log(whoRandom +' '+ actionRandom +' '+ whatRandom +' '+ whenRandom)
+  // console.log(`${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`)
+
+  let excuse = document.getElementById("excuse")
+  excuse.innerText = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`
+  //console.log(excuse.innerText)
 };
